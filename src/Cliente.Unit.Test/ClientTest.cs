8f13495a3161cd24f940bba9fbb6f11b15440500
Jordan Bragon <jordan.bragon@alliedbrasil.com.br>
2022-08-16 15:03:27 -0300

    Implementado testes de unidade

---
+++ src/Cliente.Unit.Test/ClientTest.cs
@@ -0,0 +1,135 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork;
+using AutoMapper;
+using Business.Interface;
+using DataAccess.Context;
+using Models.Filters;
+using Models.Infrastructure;
+using Moq;
+using Xunit;
+
+namespace Client.Unit.Test
+{
+    public class ClientTest
+    {
+        private readonly IClientService _clientService;
+        private readonly Mock<IUnitOfWork<DBContext>> _unitOfWorkMock;
+        private readonly IMapper _mapper;
+
+        public ClientTest()
+        {
+            var fixture = new TestFixture();
+
+            _clientService = fixture.ClientService;
+            _unitOfWorkMock = fixture.unitOfWorkMock;
+            _mapper = fixture.mapper;
+        }
+
+        #region CRUD
+
+        [Fact]
+        public void Client_Create_Success()
+        {
+            var Client = new Models.Business.Client()
+            {
+                CPF = "456.462.300-12",
+                Name = "João",
+                State = "MG",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            var result = _clientService.Create<Models.Business.Client>(Client);
+
+            Assert.True(result != null);
+            Assert.IsType<Models.Business.Client>(result);
+            _unitOfWorkMock.Verify(uow => uow.SaveChanges(false), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void Client_Get_Success(int Id)
+        {
+            var result = _clientService.GetById<Models.Business.Client>(Id);
+
+            Assert.True(result != null);
+            Assert.True(result.Id == Id);
+            Assert.IsType<Models.Business.Client>(result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void Client_Get_Null(int Id)
+        {
+            var result = _clientService.GetById<Models.Business.Client>(Id);
+
+            Assert.True(result == null);
+        }
+
+        [Fact]
+        public void Client_Search_Success()
+        {
+            var filter = new ClientFilter()
+            {
+                CPF = "869.669.460-01"
+            };
+
+            var result = _clientService.Search<Models.Business.Client>(filter);
+
+            Assert.NotNull(result);
+            Assert.True(result.Items.Any());
+            Assert.Equal(1, result.Items?.FirstOrDefault()?.Id);
+            Assert.IsType<List<Models.Business.Client>>(result.Items);
+            Assert.IsType<SearchResponse<Models.Business.Client>>(result);
+        }
+
+        [Fact]
+        public void Client_Search_Null()
+        {
+            //Arrange
+            var filter = new ClientFilter()
+            {
+                CPF = "123456789"
+            };
+
+            //Act
+            var result = _clientService.Search<Models.Business.Client>(filter);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.False(result.Items.Any());
+            Assert.IsType<List<Models.Business.Client>>(result.Items);
+            Assert.IsType<SearchResponse<Models.Business.Client>>(result);
+        }
+
+        [Fact]
+        public void Client_Update_Success()
+        {
+            var Client = new Models.Business.Client()
+            {
+                Id = 1,
+                CPF = "869.669.460-01",
+                Name = "João",
+                State = "MG",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            var result = _clientService.Update<Models.Business.Client>(Client);
+
+            Assert.True(result != null);
+            Assert.IsType<Models.Business.Client>(result);
+            _unitOfWorkMock.Verify(uow => uow.SaveChanges(false), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void Client_Delete_Success(int Id)
+        {
+            _clientService.Delete(Id);
+
+            _unitOfWorkMock.Verify(uow => uow.SaveChanges(false), Times.Once);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Cliente.Unit.Test/TestFixture.cs
@@ -0,0 +1,70 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork;
+using AutoMapper;
+using Business.Interface;
+using Business.Services;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Moq;
+using Moq.AutoMock;
+
+namespace Client.Unit.Test
+{
+    public class TestFixture
+    {
+        public readonly IClientService ClientService;
+        public readonly Mock<IUnitOfWork<DBContext>> unitOfWorkMock;
+        public readonly Mock<IRepository<Models.Business.Client>> repositoryMock;
+        public readonly IMapper mapper;
+
+        public TestFixture()
+        {
+            repositoryMock = new Mock<IRepository<Models.Business.Client>>();
+            var context = new Mock<DBContext>();
+            var dbFacade = new Mock<DatabaseFacade>(context.Object);
+
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+            });
+
+            mapper = mockMapper.CreateMapper();
+
+            var client = new Models.Business.Client()
+            {
+                Id = 1,
+                CPF = "869.669.460-01",
+                Name = "José",
+                State = "MG",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            List<Models.Business.Client> entitybases = new List<Models.Business.Client>() { client };
+
+            var dbSetMock = new Mock<DbSet<Models.Business.Client>>();
+
+            dbSetMock.As<IQueryable<Models.Business.Client>>().Setup(x => x.Provider).Returns(entitybases.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<Models.Business.Client>>().Setup(x => x.Expression).Returns(entitybases.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<Models.Business.Client>>().Setup(x => x.ElementType).Returns(entitybases.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<Models.Business.Client>>().Setup(x => x.GetEnumerator()).Returns(entitybases.AsQueryable().GetEnumerator());
+
+            context.Setup(x => x.Set<Models.Business.Client>()).Returns(dbSetMock.Object);
+
+            var repository = new Repository<Models.Business.Client>(context.Object);
+
+            var dbContextTransaction = new Mock<IDbContextTransaction>();
+
+            AutoMocker mocker = new AutoMocker();
+            mocker.GetMock<IUnitOfWork<DBContext>>().Setup(uow => uow.GetRepository<Models.Business.Client>(false)).Returns(repository);
+            mocker.GetMock<IUnitOfWork<DBContext>>().Setup(uow => uow.DbContext.Database).Returns(dbFacade.Object);
+            mocker.GetMock<IUnitOfWork<DBContext>>().Setup(uow => uow.DbContext.Database.BeginTransaction()).Returns(dbContextTransaction.Object);
+            mocker.GetMock<IUnitOfWork<DBContext>>().Setup(uow => uow.DbContext.Set<Models.Business.Client>()).Returns(dbSetMock.Object);
+
+            unitOfWorkMock = mocker.GetMock<IUnitOfWork<DBContext>>();
+
+            ClientService = new ClientService(unitOfWorkMock.Object, mapper);
+        }
+
+    }
+}
