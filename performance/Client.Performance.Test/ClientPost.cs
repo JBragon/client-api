687d2a6d02ec3087a9d028d2a17e319d40db7e12
Jordan Bragon <jordan.bragon@alliedbrasil.com.br>
2022-08-17 04:04:41 -0300

    Teste de Performance Implementado com NBomber

---
+++ performance/Client.Performance.Test/ClientPost.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Performance.Test
+{
+    public class ClientPost
+    {
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ performance/Client.Performance.Test/Program.cs
@@ -0,0 +1,51 @@
+﻿using Billing.Performance.Test;
+using Client.Performance.Test;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using System.Net.Http.Json;
+
+namespace NBomberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+
+            httpClient.BaseAddress = new Uri("https://localhost:7290/");
+
+            var step = Step.Create("GetClients", async context =>
+            {
+                var response = await httpClient.GetAsync("api/Client");
+
+                return response.IsSuccessStatusCode
+                    ? Response.Ok()
+                    : Response.Fail();
+            });
+
+            var step2 = Step.Create("PostClient", async context =>
+            {
+                ClientPost post = new ClientPost
+                {
+                    CPF = CPFUtils.GerarCpf(),
+                    Estado = "PT",
+                    Nome = $"PerformanceTest-Client {Guid.NewGuid()}"
+                };
+
+                var response = await httpClient.PostAsJsonAsync($"api/Client", post);
+
+                return response.IsSuccessStatusCode
+                    ? Response.Ok()
+                    : Response.Fail();
+            });
+
+            NBomberRunner
+                .RegisterScenarios(ScenarioBuilder.CreateScenario("GetClientsRequest", step))
+                .Run();
+
+            NBomberRunner
+                .RegisterScenarios(ScenarioBuilder.CreateScenario("PostClientRequest", step2))
+                .Run();
+        }
+    }
+}
